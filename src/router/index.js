0cb596f5a1873a39e5cf2e7340c676d5c8fc49a8
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-12-02 09:00:21 -0500

    Use webHashHistory router in prod

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import Quiz from '../components/Quiz.vue'
 import Main from '../components/Main.vue'
 import ChooseSubject from '../components/ChooseSubject.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: process.env.NODE_ENV === 'production' ? createWebHashHistory(import.meta.env.BASE_URL) : createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/quiz/:category/:name',
