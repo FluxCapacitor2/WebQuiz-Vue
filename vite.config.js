f397e11be12e1a4190792c141c8600a1c4d0c380
FluxCapacitor <31071265+FluxCapacitor2@users.noreply.github.com>
2023-01-20 13:48:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/WebQuiz/" : "/",
+  base: (process.env.NODE_ENV === "production" && process.env.VERCEL != "1") ? "/WebQuiz/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
